2bb004b9063a1e4e28b17380025906f0ba64040f
rauljfb <rjfb91@gmail.com>
2024-07-11 21:31:40 -0500

    modificacion linea viewholder

--- app/src/main/java/adapter/bebidasviewholder.kt
+++ app/src/main/java/adapter/bebidasviewholder.kt
@@ -15,6 +15,7 @@ class bebidasviewholder (view:View):RecyclerView.ViewHolder(view){
         binding.origen.text= bebidasA.origen
         Glide.with(binding.foto.context).load(bebidasA.foto).into(binding.foto)
         itemView.setOnClickListener { Toast.makeText(itemView.context, "500 pesos", Toast.LENGTH_SHORT).show()
+       //hola
         }
     }
 }
