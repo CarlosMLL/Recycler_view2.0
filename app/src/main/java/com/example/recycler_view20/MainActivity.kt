8f8ce0d947cdbaa332224e29d1ddc3cc7af1ba64
rauljfb <rjfb91@gmail.com>
2024-07-29 21:43:08 -0500

    flecha negra

--- app/src/main/java/com/example/recycler_view20/MainActivity.kt
+++ app/src/main/java/com/example/recycler_view20/MainActivity.kt
@@ -9,9 +9,6 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recycler_view20.databinding.ActivityMainBinding
-import adapter.adapter2
-import adapter.bebidasA2
-
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -21,31 +18,22 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initRecyclerView3()
         initRecyclerView()
-       initRecyclerView2()
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
-    private fun initRecyclerView3() {
-        binding.Recyclerview2
-    }
+
         private fun initRecyclerView(){
-            binding.Recyclerview
+            binding.Recyclerview.layoutManager = LinearLayoutManager(this)
+            binding.Recyclerview.adapter= bebidasadapter(bebidasProvider.bebida) {clickSelected(it)}
 
         }
-    private fun initRecyclerView2() {
-        binding.Recyclerview.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
-       binding.Recyclerview2.layoutManager=LinearLayoutManager(this)
-        binding.Recyclerview.adapter= bebidasadapter(bebidasProvider.bebida) {clickSelected(it) }
-       binding.Recyclerview2.adapter=adapter2(bebidasA2.bebidas3)
-    }
-   fun clickSelected(bebidasA: bebidasA){
+    fun clickSelected(bebidasA: bebidasA){
 Toast.makeText(this, "500 pesos", Toast.LENGTH_SHORT).show()
     }
+//hola mundo
 }
 //hoy
